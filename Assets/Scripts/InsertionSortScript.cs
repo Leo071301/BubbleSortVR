2248583d775ed9c47faada44ebcd95a8357ab12d
Leo-0713 <leo.junior.0713@gmail.com>
2025-07-09 17:12:27 -0500

    Before script finish

--- Assets/Scripts/InsertionSortScript.cs
+++ Assets/Scripts/InsertionSortScript.cs
@@ -11,9 +11,27 @@ public class InsertionSortScript : MonoBehaviour
     [SerializeField] public LiveAudioUtility liveAudio;     // script enables syncing of audio
 
     [SerializeField] public List<int> number_list; // list given through the inspector tab
+    [SerializeField] public int Total_Spacing = 15; // spacing between the cubes 
+
+    [SerializeField] public Color default_color; // initialize default color 
+    [SerializeField] public Material material; // pass in URP shader since spatial SDK does not give it material when cubes are spontaneously made 
+
+    [SerializeField] public Color Good_Color; // this color highlights both if not out of order
+    [SerializeField] public Color Swap_Color; // this color highlights when they need to swap
+    [SerializeField] public float Swap_TIME = 2.0f; // this determines how long it takes to swap
+
+    [SerializeField] public Color Check_Color; // color that highlights when its checking if it needs to be swapped
+    [SerializeField] public float Check_TIME = 1.0f; // time it takes to check if it needs to swap
+
+    [NonSerialized]
+    private List<GameObject> bubblesort_cubes = null; // list of cubes made, positioned, and programmed
+    private bool isAnimating = false; // boolean that will check to make sure multiple animations are not going over each other
+
+
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
