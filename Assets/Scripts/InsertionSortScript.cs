b4a269f800fb18e4ce13cc854ea157e04cc02096
Leo-0713 <leo.junior.0713@gmail.com>
2025-10-27 21:55:35 -0500

    insertion sort progress
    Added the logic of insertion sort basically. Just gotta add the animation of the actual shifting and inserting as well as narration and syncing audio.

--- Assets/Scripts/InsertionSortScript.cs
+++ Assets/Scripts/InsertionSortScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -28,49 +29,86 @@ public class InsertionSortScript : MonoBehaviour
     [NonSerialized]
     private List<GameObject> insertionsort_cubes = null; // list of cubes made, positioned, and programmed
     private bool isAnimating = false; // boolean that will check to make sure multiple animations are not going over each other
+    GlowHandler glowHandler;
 
+    private const float text_speed = 0.25f;
 
     public IEnumerator InsertionSortAnimation()
     {
         isAnimating = true; // animation has officially started
+        liveText.syncLiveText((int)text.NO_HIGHLIGHT); // tell LiveTextUtility to show text at index 0 (text with no code highlighted)
+
+
+
         yield return StartCoroutine(CubeUtility.AnimateSpawnCubes(insertionsort_cubes, this));
         yield return StartCoroutine(textPanel.SpawnIn());
 
+        int n = insertionsort_cubes.Count;
+
+        liveText.syncLiveText((int)text.N_LEN); // time to highlight n=len(arr)
+
+        for (int i = 1; i < n; i++)
+        {
+            // remove glow from all cubes, then apply glow to only the unsorted portion, starting from cube i to the end 
+            glowHandler.ResetApplyGlowMaterial(insertionsort_cubes, insertionsort_cubes.Skip(i).ToList());
+
+            int key = int.Parse(insertionsort_cubes[i].name);
+            int j = i - 1;
 
 
+            yield return liveText.syncLiveTextWait((int)text.FOR_I, text_speed * 2);
+            yield return liveText.syncLiveTextWait((int)text.KEY, text_speed * 2);
+            yield return liveText.syncLiveTextWait((int)text.J_EQUALS_I, text_speed * 2);
+            // should the while loop be highlighted at this point? 
+
+            // Highlight the two cubes being compared 
+            yield return StartCoroutine(CubeUtility.PulseHighlight(insertionsort_cubes[j + 1], insertionsort_cubes[j], Check_Color, Check_TIME));
+
+            while (j >= 0 && int.Parse(insertionsort_cubes[j].name) > key)
+            {
+
+                // animate cube shifting here?? and insertion? 
+                // add narration of WHILE_J / SHIFT ? INSERT
+                insertionsort_cubes[j + 1] = insertionsort_cubes[j]; // would a c# fast swap work here? 
+                j = j - 1;
+            }
+
+            // insertionsort_cubes is a list of game objects, 
+            // insertionsort_cubes[j] gives the cube itself, type is game object 
+            // .name is the string that stores the objects name
+            // key is an int, so we use ToString() to convert integar into string version 
+            // taking the cubes name property (string) and giving it a new string value derived from integar key 
+            // IDEA: Place the key into its correct position (j + 1) in the sorted portion
+            insertionsort_cubes[j+1].name = key.ToString(); // set j to key (need to update key as string?)
+
+        }
+
+        liveText.syncLiveText((int)text.NO_HIGHLIGHT);
+        glowHandler.ResetAllGlow(insertionsort_cubes);
+
+
+        // cool finished-animation
+        foreach (var cube in insertionsort_cubes)
+        {
+            StartCoroutine(CubeUtility.PulseHighlight(cube, Good_Color, 1.0f));
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        // and destroy these cubes!!
         yield return StartCoroutine(CubeUtility.AnimateDestroyCubes(insertionsort_cubes, this));
         insertionsort_cubes = null;
         // panel goes away
         yield return StartCoroutine(textPanel.Despawn());
 
-
         isAnimating = false;
 
-        /*int n = insertionsort_cubes.Count; // gets amount of cubes that exist 
-
-
-        for (int i = 1; i < n; i++) {
-            int insert_index = i; // gets current index
-            int current_value = int.Parse(insertionsort_cubes[insertionsort_cubes.Count - 1].name); // gets last element before its removed
-            insertionsort_cubes.RemoveAt(insertionsort_cubes.Count - 1); // removes last element
-            for(int j = i - 1; j > -1; j--)
-            {
-                if (int.Parse(insertionsort_cubes[j].name) > current_value)
-                {
-                    insert_index = j;
-
-                }
-            }
-            insertionsort_cubes.Insert(insert_index, insertionsort_cubes[current_value]);
-        }
-        yield return null;
-        isAnimating = false; // no longer animating */
 
     }
 
     public void InsertionSortEvent()
     {
         if (isAnimating) return; // prevents multiple events at once
+
 
 
 
@@ -82,7 +120,11 @@ public class InsertionSortScript : MonoBehaviour
             CubeUtility.positionCubeList(insertionsort_cubes, Total_Spacing, this);
         }
 
-        // initialize glow handler here? 
+        // initialize glow handler
+        glowHandler = new GlowHandler();
+        glowHandler.material_normal = material;
+        glowHandler.material_glow = material_glow;
+        glowHandler.Init(insertionsort_cubes);  // VERY IMPORTAINT 
 
         StartCoroutine(InsertionSortAnimation());
     }
@@ -90,8 +132,7 @@ public class InsertionSortScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        liveText.syncLiveText(0);
-        Debug.Log("Start method has started.");
+        liveText.syncLiveText((int)text.NO_HIGHLIGHT);
 
 
     }
@@ -118,5 +159,24 @@ public class InsertionSortScript : MonoBehaviour
         Audio4,
         Audio5 = 5
 
+
     }
+
+    public enum text
+    {
+        NO_HIGHLIGHT = 0,
+        N_LEN,
+        FOR_I,
+        KEY,
+        J_EQUALS_I,
+        WHILE_J,
+        ARR_J,
+        J_MINUS,
+        ARR_J_KEY = 7
+
+    }
+
+
+
+    
 }
